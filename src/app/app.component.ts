d556f18ef553b1f3d0d486b64aedbe30539b0d32
Cole Cassidy <colewcassidy@gmail.com>
2022-03-27 12:15:38 -0400

    Add Bootstrap
    Easier done than said.... so far.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  title = 'iota-ang-app';
+  title = 'Iota (beta)';
+  button = 'clickme';
 }
